befd50897f421f2782b886b6535ce0cba78d47de
Victor Marra <zaffalonvictor@gmail.com>
2021-05-02 18:05:57 -0300

    Created first version of 2d board

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { List, Avatar } from "antd";
+import { useState } from "react";
+import "antd/dist/antd.css";
+
+const Cell = styled.div`
+  background: #ffffff;
+  height: 60px;
+  border-style: solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: -8px;
+  background: "white";
+`;
+
+const BoardBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Board = styled.div`
+  margin-top: 80px;
+  width: 800px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`;
 
 function App() {
+  const col = 10;
+  const row = 10;
+  const [board, setBoard] = useState(Array(col * row).fill(0));
+
+  const setUserLocation = (index: number) => {
+    let arrayCopy = Array(col * row).fill(0);
+    arrayCopy[index] = 1;
+    setBoard(arrayCopy);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BoardBackground>
+        <Board>
+          <List
+            style={{ flexDirection: "row" }}
+            grid={{ gutter: 0, column: col }}
+            dataSource={board}
+            renderItem={(item, index) => (
+              <>
+                <List.Item style={{ margin: 0 }}>
+                  <Cell onClick={() => setUserLocation(index)} title={"pica"}>
+                    {item == 1 && (
+                      <Avatar
+                        style={{
+                          backgroundColor: "#f56a00",
+                          verticalAlign: "middle",
+                        }}
+                        size="large"
+                        gap={4}
+                      >
+                        Victor
+                      </Avatar>
+                    )}
+                  </Cell>
+                </List.Item>
+              </>
+            )}
+          />
+        </Board>
+      </BoardBackground>
     </div>
   );
 }
