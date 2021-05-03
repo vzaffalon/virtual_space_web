2700939593ce462b42bb01ef1fd61e5959b8a139
Victor Marra <zaffalonvictor@gmail.com>
2021-05-03 14:11:54 -0300

    Add messages to messages list logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,30 @@ import styled from "styled-components";
 import { List, Avatar, Button, Input } from "antd";
 import { useState, useEffect } from "react";
 import floor from "./floor.png";
+import { Users } from "./interfaces/user/users.interface";
+import { User } from "./interfaces/user/user.interface";
 import "antd/dist/antd.css";
 import socketIOClient from "socket.io-client";
+import { Message } from "./interfaces/message/message.interface";
 const ENDPOINT = "http://127.0.0.1:4001";
 
 const Cell = styled.div`
   background: url(${floor});
   height: 60px;
-  border-style: solid;
+  border-style: 1px solid;
   display: flex;
   align-items: center;
   justify-content: center;
   margin-bottom: -8px;
 `;
+
+const ChatMessage = styled.div`
+  background: white;
+  height: 80px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`
 
 const BoardBackground = styled.div`
   width: 100%;
@@ -42,6 +53,7 @@ function App() {
   const row = 10;
   const [board, setBoard] = useState(Array(col * row).fill({}));
   const [messages, setMessages] = useState([]);
+  const [newMessages, setNewMessages] = useState([]);
 
   const setUserLocation = (index: number) => {
     let arrayCopy = Array(col * row).fill(0);
@@ -49,21 +61,35 @@ function App() {
     setBoard(arrayCopy);
   };
 
+  const getMessages = () => {
+
+  }
+
+  const getUsers = () => {
+    
+  }
+
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
-    socket.on("messages", (messages) => {
-      let arrayCopy = [...board];
-      messages.map((message: any) => (arrayCopy[user.position] = user));
-      setBoard(arrayCopy);
-      setMessages(data);
+    socket.on("messages", (message: Message) => {
+      let newMessagesCopy: any = [...newMessages];
+      let messagesCopy: any = [...messages];
+      newMessagesCopy.push(message)
+      messagesCopy.push(message)
+      setMessages(newMessagesCopy);
+      setNewMessages(messagesCopy);
     });
 
-    socket.on("users", (users) => {
-      let arrayCopy = [...board];
-      users.map((user: any) => (arrayCopy[user.position] = user));
-      setBoard(arrayCopy);
+    socket.on("users", (users: Users) => {
+      let newUsers = Array(col * row).fill({})
+      users.map((user: User) => (newUsers[user.position] = user));
+      setBoard(newUsers);
     });
   }, []);
+
+  const findLastMessage = (user_id: string) => {
+    return messages.find((message: Message) => message.user_id = user_id)
+  }
 
   return (
     <div>
@@ -77,16 +103,19 @@ function App() {
                 <List.Item style={{ margin: 0 }}>
                   <Cell onClick={() => setUserLocation(index)} title={"pica"}>
                     {item && item.name && (
-                      <Avatar
-                        style={{
-                          backgroundColor: "#f56a00",
-                          verticalAlign: "middle",
-                        }}
-                        size="large"
-                        gap={4}
-                      >
-                        {item.name}
-                      </Avatar>
+                      <div>
+                        <Avatar
+                          style={{
+                            backgroundColor: "#f56a00",
+                            verticalAlign: "middle",
+                          }}
+                          size="large"
+                          gap={4}
+                        >
+                          {item.name}
+                        </Avatar>
+                        {findLastMessage(item._id) && <ChatMessage>{findLastMessage(item._id).content}</ChatMessage>}
+                      </div>
                     )}
                   </Cell>
                 </List.Item>
